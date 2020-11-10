1b137a8e5246fedfda04b9087ce7befc253ab498
ZacharyAkers <zakhariakers@gmail.com>
2020-11-09 19:30:52 -0500

    2nd part of small done

---
+++ hello2.py
@@ -0,0 +1,8 @@
+name = input('What is your name? ')
+length_of_name = len(name)
+greeting = "Hello, " + name
+counter1 = "your name has "
+counter2 = " letters in it! awesome!"
+total_counter = counter1 + str(length_of_name) + counter2
+print(greeting.upper())
+print(total_counter.upper())
