ab4dd601af60da38fb29b145ded0901aba435194
ZacharyAkers <zakhariakers@gmail.com>
2020-11-09 20:30:39 -0500

    4th small exercise done

---
+++ small_exercises/day_of_week.py
@@ -0,0 +1,17 @@
+day = int(input('Day (0-6)? '))
+
+if day >= 1:
+    if day == 1:
+        print("Monday")
+    if day == 2:
+        print("Tuesday")
+    if day == 3:
+        print("Wednesday")
+    if day == 4:
+        print("Thursday")
+    if day == 5:
+        print("Friday")
+    if day == 6:
+        print("Saturday")
+else:
+    print("Sunday")
